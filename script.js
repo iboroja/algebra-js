539cddf3a14e030c661da831a1319b4a3d284be2
iboroja <iboroja@gmail.com>
2023-01-26 21:06:59 +0100

    First commit

---
+++ script.js
@@ -0,0 +1,56 @@
+/*  Inicijalizacija varijabli?
+
+var first_name = "Tony";
+var first_name = "John";
+var lastName = "Doe";
+var price = 0;
+var _0discount = 78;
+var fullPrice = 0;
+var _break = [];
+var $myObject = {};
+
+console.log(price);
+console.log(lastName);
+
+
+Kraj mog
+programa 
+*/
+
+
+/*1. Nađite varijable u listi koje su iste po vrijednosti.
+2. Koja je vrijednost varijable x na kraju programa? Koji je to tip podatka?
+3. Koja je vrijednost varijable baaa? Objasnite!
+4. Provjerite tip podatka varijable w.*/
+/*
+var x = "Lorem ipsum",
+    y = 2345,
+    z = "2345",
+	q = false,
+	w;
+
+x += '3' + 1;
+var baaa = "b" + "a" + +"a" + "a";
+
+console.log(x);
+console.log(typeof w);*/
+
+
+
+
+var text = ' Lorem ipsum dolor sit amet';
+
+
+console.log(text.length);
+
+console.log(text.replace('amet', 'elit'));
+
+console.log(`${text} + "consectetur adipiscing elit"`);
+
+console.log(text.toUpperCase());
+
+console.log(text.trim());
+
+console.log(text.charAt(12));
+
+console.log(text.indexOf("s"));
